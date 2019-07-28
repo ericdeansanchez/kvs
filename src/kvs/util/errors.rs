e290b4885fc994238e33bb52854c1ef327f7224f
ericdeansanchez <ericdeansanchez@berkeley.edu>
2019-07-28 11:47:55 -0700

    digging into c using bifrost

--- src/kvs/util/errors.rs
+++ src/kvs/util/errors.rs
@@ -1,5 +1,26 @@
 //! Primary error structures for kvs.
+use std::io;
+
 use failure;
+
+pub enum KvsError {
+    Io(io::Error),
+    Serde(serde_json::Error),
+    //KeyNotFound(String),
+    //UnexpectedCommandType(String),
+}
+
+impl From<io::Error> for KvsError {
+    fn from(err: io::Error) -> KvsError {
+        KvsError::Io(err)
+    }
+}
+
+impl From<serde_json::Error> for KvsError {
+    fn from(err: serde_json::Error) -> KvsError {
+        KvsError::Serde(err)
+    }
+}
 
 /// Use failure crate until obvious error types emerge.
 /// Use of failure is discouraged; its use here is
